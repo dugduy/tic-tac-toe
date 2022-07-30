3c480368bbfec32e32e1ec0a64de1229aa7ea9e3
dug duy <dugduy01@gmail.com>
2022-07-30 18:36:26 +0700

    improve some btns

--- a.py
+++ a.py
@@ -20,13 +20,13 @@ def on_click(i):
     crplayer=players[player_turn%2]
     on_play[crplayer].append(i)
     btn_list[i].config(text=crplayer)
-    btn_list[i].config(state=DISABLED)
+    btn_list[i].config(state=DISABLED,borderwidth=1)
     if win_check():
         for btn in btn_list:
             btn.config(state=DISABLED)
         showinfo('Game over!',crplayer+' win!\nhehe')
     player_turn+=1
 for i in range(lv*lv):
-    btn_list.append(Button(width=10,height=5,command=lambda i=i:on_click(i)))
+    btn_list.append(Button(width=10,height=5,fg='red',borderwidth=5,font=(0,20),command=lambda i=i:on_click(i)))
     btn_list[-1].grid(row=int(i/lv),column=i%lv)
 root.mainloop()
